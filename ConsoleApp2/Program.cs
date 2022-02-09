0f6b380005c5dca6b37cd5db7d4161b9fc1f63df
Ulkis96 <uliankirilov2004@gmail.com>
2022-02-09 09:07:47 +0200

    zadacha

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arr = Console.ReadLine().Split().ToArray();
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    string[] cmd = Console.ReadLine().Split().ToArray();
+
+    string command = cmd[0];
+    switch (command)
+    {
+        case "Reverse":
+            arr = arr.Reverse().ToArray();
+            break;
+        case "Distinct":
+            arr = arr.Distinct().ToArray();
+            break;
+        case "Replace":
+            int index = int.Parse(cmd[1]);
+            string newString = cmd[2];
+            arr[index] = newString;
+            break;
+        default:
+            break;
+    }
+}
+Console.WriteLine(string.Join(", ", arr));
