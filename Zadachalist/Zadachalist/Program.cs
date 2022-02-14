a011985384727669ca805070c9f3683e382d6f5e
Ulkis96 <uliankirilov2004@gmail.com>
2022-02-14 09:12:37 +0200

    zdr

---
+++ Zadachalist/Zadachalist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadachalist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
